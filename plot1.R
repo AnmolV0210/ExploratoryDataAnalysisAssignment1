ba889579dc67b97a8259f0cc5a5433f2b59c92a6
AnmolV0210 <anmolv0210@gmail.com>
2020-04-27 13:30:16 +0530

    Plots and corresponding R code for Peer-Graded Assignment Week1  1

---
+++ plot1.R
@@ -0,0 +1,35 @@
+##Loading libraries
+library(data.table)
+library(dplyr)
+
+##Extratcing data from website
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("dataset.zip"))
+        download.file(url,destfile = "dataset.zip", method = "curl")
+if(!file.exists("household_power_consumption.txt"))
+        unzip("dataset.zip")
+
+## Getting the data
+data <- data.table::fread("household_power_consumption.txt", na.strings = "?")
+
+## Removing null values
+data <- na.omit(data)
+
+##Combinig date and time columns
+data <- data %>% mutate(DT = paste(Date,Time))
+
+##Change DT and Date from chr to Date/Time classs objects
+data$DT <- strptime(data$DT, format="%d/%m/%Y %H:%M:%S")
+data$Date <- strptime(data$Date, format="%d/%m/%Y")
+
+##Subset 2 days i.e 01/02/2007 and 02/02/2007
+data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+
+##Plot the graph on/using png graphic device
+
+png(filename = "plot1.png", width = 480, height = 480)
+hist(data$Global_active_power, xlab = "Global Active Power(kilowatts)", main = "Global Active Power", col = "red")
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,38 @@
+##Loading libraries
+library(data.table)
+library(dplyr)
+
+##Extratcing data from website
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!file.exists("dataset.zip"))
+   download.file(url,destfile = "dataset.zip", method = "curl")
+if(!file.exists("household_power_consumption.txt"))
+   unzip("dataset.zip")
+
+## Getting the data
+data <- data.table::fread("household_power_consumption.txt", na.strings = "?")
+
+## Removing null values
+data <- na.omit(data)
+
+##Combinig date and time columns
+data <- data %>% mutate(DT = paste(Date,Time))
+
+##Change DT and Date from chr to Date/Time classs objects
+data$DT <- strptime(data$DT, format="%d/%m/%Y %H:%M:%S")
+data$Date <- strptime(data$Date, format="%d/%m/%Y")
+
+##Subset 2 days i.e 01/02/2007 and 02/02/2007
+data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+
+##Plot the graph on/using png graphic device
+png(filename = "plot3.png", width = 480, height = 480)
+plot(data$DT,data$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub meeting")
+lines(data$DT,data$Sub_metering_1, type = "l")
+lines(data$DT,data$Sub_metering_2, type = "l", col = "red")
+lines(data$DT,data$Sub_metering_3, type = "l", col = "blue")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.75, lty=c(1,1,1), lwd=c(2.5,2.5,2.5), col=c("black","red","blue"))
+
+dev.off()
